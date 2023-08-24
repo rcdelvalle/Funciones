66ec26128a60f936e0791096eee6d191d685a201
rcdelvalle <rcdelvalle2017@gmail.com>
2023-08-24 10:54:07 -0300

    Ejercicio 3

--- Funciones.py
+++ Funciones.py
@@ -14,4 +14,16 @@ def area_circulo(radio):
     return area
 
 print(area_circulo(2))
+
+
+#Ejercicio 3.
+def relacion(num1,num2):
+    if num1 > num2:
+        return 1
+    elif num1 < num2:
+        return -1
+    else:
+        return 0
+    
+print(relacion(7,3))
     
