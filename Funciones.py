732d22b50c266226a982902ee3931261ff0578d1
rcdelvalle <rcdelvalle2017@gmail.com>
2023-08-24 10:43:41 -0300

    Ejercicio 2

--- Funciones.py
+++ Funciones.py
@@ -4,4 +4,14 @@ def area_rectangulo(base, altura):
     return area
 
 print(area_rectangulo(4,5))
+
+
+#Ejercicio 2.
+PI = 3.14159
+
+def area_circulo(radio):
+    area = PI * (radio**2)
+    return area
+
+print(area_circulo(2))
     
